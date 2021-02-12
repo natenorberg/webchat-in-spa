6c64b1d2d6a6e907ce19eee3de40ca06f0f6cfc6
Nate Norberg <nate.norberg@goquiq.com>
2021-02-12 10:30:00 -0700

    Seeing if this works

--- src/quiqChatConfig.js
+++ src/quiqChatConfig.js
@@ -47,6 +47,6 @@
       };
     })(window.history);
   };
-  script.src = `https://${tenant}.quiq-api.com/app/webchat/index.js`;
+  script.src = `https://nate.quiq.dev:3000/app/webchat/index.js`;
   document.head.appendChild(script);
 })();
